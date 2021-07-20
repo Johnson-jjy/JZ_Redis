6d4d8b8b7433e77e74538728683ae441657b4c87
unknown <2251697603@qq.com>
2021-07-20 20:01:13 +0800

    tcp

---
+++ tcp/server.go
@@ -0,0 +1,81 @@
+package tcp
+
+import (
+	"JZ_Redis/interface/tcp"
+	"context"
+	"fmt"
+	"godis/lib/logger"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+// A tcp server
+
+// Config stores tcp server properties
+type Config struct {
+	Address string `yaml:"address"`
+	MaxCount uint32 `yaml:"max-connect"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+// ListenAndServeWithSignal binds port and handle requests, blocking until receive stop signal
+func ListenAndServerWithSignal(cfg *Config, handler tcp.Handler) error {
+	closeChan := make(chan struct{})
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigCh
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeChan <-struct{}{}
+		}
+	}()
+	listener, err := net.Listen("tcp", cfg.Address)
+	if err != nil {
+		return err
+	}
+	//cfg.Address = listener.Addr().String()
+	logger.Info(fmt.Sprintf("bind: %s, start listening...", cfg.Address))
+	ListenAndServe(listener, handler, closeChan)
+	return nil
+}
+
+// ListenAndServe binds port and handle requests, blocking until close
+func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
+	// listen signal
+	go func() {
+		<-closeChan
+		logger.Info("shutting down...")
+		_ = listener.Close() // listener.Accept() will return err immediately
+		_ = handler.Close() // close connection
+	}()
+
+	// listen port
+	defer func() {
+		// close during unexpected error
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+	ctx := context.Background()
+	var waitDone sync.WaitGroup
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			break
+		}
+		// handle
+		logger.Info("accept link")
+		waitDone.Add(1)
+		go func() {
+			defer func() {
+				waitDone.Done()
+			}()
+			handler.Handle(ctx, conn)
+		}()
+	}
+	waitDone.Wait()
+}
